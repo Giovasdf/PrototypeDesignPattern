c4145f68bbccd0782420669e5aa7c82629944545
Giovanni Molina <giov.molina@alumnos.duoc.cl>
2020-06-05 00:23:56 -0400

    First Commit

---
+++ TestPrototype/Assets/GetFileByTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class GetFileByTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    
+
+}
